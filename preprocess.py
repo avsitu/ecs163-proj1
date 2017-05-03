8426564274426bc134ebe09dd6b29366f884854c
Alvin Situ <jisitu@ucdavis.edu>
2017-05-03 00:29:03 -0700

    generate data for graph 3

--- preprocess.py
+++ preprocess.py
@@ -14,7 +14,7 @@ def ImportTable():
 	  "satisfaction_level" real,
 	  "last_evaluation" real,
 	  "number_project" int,
-	  "average_montly_hours" int,	
+	  "average_monthly_hours" int,	
 	  "time_spend_company" int,
 	  "Work_accident" int,
 	  "is_left" int,
@@ -27,12 +27,12 @@ def ImportTable():
 	cursor.execute("COPY ecs163_proj1 FROM '/home/alvin/Downloads/HR_comma_sep.csv' DELIMITER ',' CSV HEADER;")
 	conn.commit()
 
-# ImportTable()
-cursor.execute("SELECT DISTINCT(department) FROM ecs163_proj1")
-depts = cursor.fetchall()
-for d in depts:
-	cursor.execute("SELECT COUNT(department) FROM ecs163_proj1 WHERE department='%s';"%d[0])
-	total = cursor.fetchone()[0]
-	cursor.execute("SELECT COUNT(department) FROM ecs163_proj1 WHERE department='%s' AND is_left=1;"%d[0])
-	left = cursor.fetchone()[0]
-	print '%s: %f' %(d[0],float(left)/total)
+ImportTable()
+# cursor.execute("SELECT DISTINCT(department) FROM ecs163_proj1")
+# depts = cursor.fetchall()
+# for d in depts:
+# 	cursor.execute("SELECT COUNT(department) FROM ecs163_proj1 WHERE department='%s';"%d[0])
+# 	total = cursor.fetchone()[0]
+# 	cursor.execute("SELECT COUNT(department) FROM ecs163_proj1 WHERE department='%s' AND is_left=1;"%d[0])
+# 	left = cursor.fetchone()[0]
+# 	print '%s: %f' %(d[0],float(left)/total)
